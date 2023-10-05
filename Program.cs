f08a8a3bf084e47475cf268b5d6538724564d079
Natalia Khasanova <natell_2005@mail.ru>
2023-10-05 15:46:54 +0300

    задача 36

--- Program.cs
+++ Program.cs
@@ -1,32 +1,67 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// int count = 0;
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//     count++;
+// }
+
+
+// Console.WriteLine($"количество чётных чисел в массиве -> {count} ");
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int size = 4;
 int[] numbers = new int[size];
-int count = 0;
 FillArrayRandomNumbers(numbers);
 PrintArray(numbers);
 
+int sumNumbersEvenIndex = 0;
 
-for (int i = 0; i < numbers.Length; i++)
+for (int i = 1; i < numbers.Length; i += 2)
 {
-    if (numbers[i] % 2 == 0)
-    count++;
+    sumNumbersEvenIndex += numbers[i];
 }
+Console.Write(sumNumbersEvenIndex);
 
 
-Console.WriteLine($"количество чётных чисел в массиве -> {count} ");
 
 void FillArrayRandomNumbers(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(100,1000);
+        array[i] = new Random().Next(-100, 101);
     }
 }
+
 void PrintArray(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
     }
